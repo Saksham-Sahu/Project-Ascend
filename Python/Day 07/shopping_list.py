a2ccd94d52c8ae5bb66b096df87aa612d57794cd
Saksham Sahu <sahujifinancialadvisor@gmail.com>
2026-07-22 12:10:22 +0530

    Practice lists and for loops

---
+++ Python/Day 07/shopping_list.py
@@ -0,0 +1,10 @@
+
+shopping_list = []
+
+for i in range(5):
+    item = input("Enter item: ")
+    shopping_list.append(item)
+
+print("Current shopping list:", shopping_list)
+print("Total items in the shopping list:", len(shopping_list))
+    
